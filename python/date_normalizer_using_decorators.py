4d14e8c236c072a3d89504cf29b2fee9ef67d908
owner-meraki <luisrch594@gmail.com>
2025-01-28 19:48:36 -0600

    Exercise of decorators and timezones

--- python/date_normalizer_using_decorators.py
+++ python/date_normalizer_using_decorators.py
@@ -4,6 +4,57 @@ Beggining date: 12-1-25
 
 File dedicated to normalization of dates. It uses decorators, it mostly work for exercises.
 Python version used: 3.13
+
+#must install python-dateutil first
+pip install python-dateutil
 Version: 1.0
 """
+
+from dateutil import parser
+from datetime import datetime
+import pytz
+
+
+class RegionDate():
+
+    def normalize_date(date_func):
+        def wrapper(self,*arg, **kw):
+            region_datetime=date_func(self,*arg, **kw)
+            if type(region_datetime)==datetime:
+                return region_datetime.strftime("%d-%m-%Y %H:%M:%S")
+            else:
+                return parser.parse(region_datetime).strftime("%d-%m-%Y %H:%M:%S")
+            
+        return wrapper
+
+    @normalize_date
+    def japan_time(self):
+        japan_tz = pytz.timezone('Asia/Tokyo')
+        japan_time = datetime.now(japan_tz)
+        return japan_time
+    
+    @normalize_date
+    def england_time(self):
+        england_tz=pytz.timezone("Europe/London")
+        england_time=datetime.now(england_tz)
+        return england_time
+
+    @normalize_date
+    def india_time(self):
+        #in a disordered way to later on get it ordered
+        india_tz=pytz.timezone("Asia/Kolkata")
+        india_time=datetime.now(india_tz).strftime("%Y-%m-%d %H:%M:%S")
+        return india_time
+
+    def show_all_timezones(self):
+        return pytz.all_timezones
+
+dates_around_the_world=RegionDate()
+japan_time=dates_around_the_world.japan_time()
+england_time=dates_around_the_world.england_time()
+india_time=dates_around_the_world.india_time()
+print(f"The time in Japan right now is: {japan_time}")
+print(f"The time in England right now is: {england_time}")
+print(f"The time in India right now is: {india_time}")
+
 
