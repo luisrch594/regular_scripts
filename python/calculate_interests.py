3071d99c77b5a2ba9d2d9ae42a57d11263db5afd
luisrch594 <luisrch594@gmail.com>
2025-04-29 23:21:35 -0600

    Updates to handle better quantities
    This whole script is done in spanish to help me calculate stuff with my dad and to better assess myself in case a bad calculation is done by certificates entitites

--- python/calculate_interests.py
+++ python/calculate_interests.py
@@ -1,13 +1,11 @@
 from num2words import num2words
 
 
-CONST_PLAZOS_DE_PAGO={"Mensual":1,
-                      "Semestral":2,
-                      "Vencimiento":3}
+CONST_PLAZOS_DE_PAGO={1:"Mensual",
+                      2:"Semestral",
+                      3:"Vencimiento"}
 
-
-CONST_PLAZOS_DE_PAGO_INVERTED = {1: "Mensual", 2: "Semestral", 3: "Vencimiento"}
-
+IMPUESTO_DE_LA_RENTA=7
 
 class Certificado:
     def __init__(self, monto_base,plazo_de_pago=1,plazo_en_meses_vigencia=12,tasa_de_interes=0):
@@ -19,6 +17,8 @@ class Certificado:
         self.impuestos = 0
         self.intereses_despues_de_deducciones=0
         self.pago_por_periodo=0
+        self.pago_por_periodo_sin_deducciones=0
+        self.perdida_por_impuestos=0
         
         
 
@@ -27,7 +27,7 @@ class Certificado:
 
     def calcular_intereses_y_deducciones(self):
         self.intereses=self.monto_base*self.tasa_de_interes/100
-        self.impuestos=self.intereses*15/100
+        self.impuestos=self.intereses*IMPUESTO_DE_LA_RENTA/100
         self.intereses_despues_de_deducciones=self.intereses-self.impuestos
 
         #redonde
@@ -36,31 +36,55 @@ class Certificado:
         self.intereses_despues_de_deducciones=round(self.intereses_despues_de_deducciones,2)
 
     def deposito_a_realizarse(self):
-        if self.plazo_de_pago==1:
+        if self.plazo_de_pago=="Mensual":
+            self.pago_por_periodo_sin_deducciones=self.intereses/self.plazo_en_meses_vigencia
             self.pago_por_periodo=self.intereses_despues_de_deducciones/self.plazo_en_meses_vigencia
-        elif self.plazo_de_pago==2:
+
+        elif self.plazo_de_pago=="Semestral":
             numero_de_semestres=self.plazo_en_meses_vigencia/6 #6 meses
             self.pago_por_periodo=self.intereses_despues_de_deducciones/numero_de_semestres
+            self.pago_por_periodo_sin_deducciones=self.intereses/numero_de_semestres
+
         else:
             self.pago_por_periodo=self.intereses_despues_de_deducciones
+            self.pago_por_periodo_sin_deducciones=self.intereses
+
         self.pago_por_periodo=round(self.pago_por_periodo,2)
+        self.pago_por_periodo_sin_deducciones=round(self.pago_por_periodo_sin_deducciones,2)
+        self.perdida_por_impuestos=round(self.pago_por_periodo_sin_deducciones-self.pago_por_periodo,2)
 
     def muestra_datos_del_certificado(self)->None:
+        
+        def custom_num2words(number):
+
+            try:
+                integer_part, decimal_part = str(number).split('.')
+                if decimal_part=="0":
+                    return num2words(number,lang="es")
+            except:
+                return num2words(number,lang="es")
+            integer_words = num2words(integer_part, lang='es')
+            decimal_words = num2words(decimal_part, lang='es')
+            return f"{integer_words} punto {decimal_words}"
+
+
         self.calcular_intereses_y_deducciones()
         self.deposito_a_realizarse()
         data=""
-        data+=f"\nMonto base del certificado: \u20A1{self.monto_base} {num2words(self.monto_base, lang='es')}\n"
+        data+=f"\nMonto base del certificado: \u20A1{self.monto_base} - ({custom_num2words(self.monto_base)})\n"
         data+=f"Tasa de interes: {self.tasa_de_interes}%\n"
-        data+=f"Intereses acumulados: \u20A1{self.intereses}\n"
-        data+=f"Deducciones de impuestos: \u20A1{self.impuestos}\n"
-        data+=f"Intereses despues de deducciones: \u20A1{self.intereses_despues_de_deducciones}\n\n"
+        data+=f"Intereses acumulados: \u20A1{self.intereses} - ({custom_num2words(self.intereses)})\n"
+        data+=f"Deducciones de impuestos: \u20A1{self.impuestos} - ({custom_num2words(self.impuestos)})\n"
+        data+=f"Intereses despues de deducciones: \u20A1{self.intereses_despues_de_deducciones} - ({custom_num2words(self.intereses_despues_de_deducciones)})\n\n"
         data+=f"Plazo de vigencia: {self.plazo_en_meses_vigencia} meses\n"
-        data+=f"Periodicidad de los pagos de intereses: {CONST_PLAZOS_DE_PAGO_INVERTED[self.plazo_de_pago]}\n"
-        data+=f"Pago por periodo: \u20A1{self.pago_por_periodo}\n"
+        data+=f"Periodicidad de los pagos de intereses: {self.plazo_de_pago}\n"
+        data+=f"Pago por periodo: \u20A1{self.pago_por_periodo} ({custom_num2words(self.pago_por_periodo)})\n"
+        data+=f"Pago por período sin deducciones: \u20A1{self.pago_por_periodo_sin_deducciones} ({custom_num2words(self.pago_por_periodo_sin_deducciones)})\n"
+        data+=f"Perdida por impuestos: \u20A1{self.perdida_por_impuestos} ({custom_num2words(self.perdida_por_impuestos)})\n"
         print(data)
 
 
-certificado=Certificado(monto_base=25000000,plazo_de_pago=CONST_PLAZOS_DE_PAGO["Mensual"],
+certificado=Certificado(monto_base=19000000,plazo_de_pago=CONST_PLAZOS_DE_PAGO[2],
                         plazo_en_meses_vigencia=12,
-                        tasa_de_interes=6.5)
+                        tasa_de_interes=6.55)
 certificado.muestra_datos_del_certificado()
